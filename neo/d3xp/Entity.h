954ff8a33f9eabb5cdc02ef61a378d81d3175bd5
Admer <49600504+Admer456@users.noreply.github.com>
2021-07-29 14:21:03 +0200

    idEntity::entityFlags_s: add tabs where missing

--- neo/d3xp/Entity.h
+++ neo/d3xp/Entity.h
@@ -220,9 +220,9 @@ public:
 		bool				neverDormant		: 1;	// if true the entity never goes dormant
 		bool				isDormant			: 1;	// if true the entity is dormant
 		bool				hasAwakened			: 1;	// before a monster has been awakened the first time, use full PVS for dormant instead of area-connected
-		bool				networkSync			: 1; // if true the entity is synchronized over the network
+		bool				networkSync			: 1;	// if true the entity is synchronized over the network
 		bool				grabbed				: 1;	// if true object is currently being grabbed
-		bool				skipReplication		: 1; // don't replicate this entity over the network.
+		bool				skipReplication		: 1;	// don't replicate this entity over the network.
 	} fl;
 
 	int						timeGroup;
